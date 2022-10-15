0bf4ecfedb42d03107cd1f964e4891713d1b583a
PureSpider <alex.lamprecht1@gmail.com>
2022-10-15 15:03:33 +0200

    prettierrc, formatting

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 require("dotenv-safe").config();
 
-var keypress = require('keypress');
+var keypress = require("keypress");
 keypress(process.stdin);
 
 const { Worker } = require("worker_threads");
@@ -25,10 +25,10 @@ const workerData = {};
 
 workerData.search = search;
 workerData.data = new Int32Array(
-  // 0 = attempts
-  // 1 = paused flag
-  // 2 = done
-  new SharedArrayBuffer(Int32Array.BYTES_PER_ELEMENT * 3)
+    // 0 = attempts
+    // 1 = paused flag
+    // 2 = done
+    new SharedArrayBuffer(Int32Array.BYTES_PER_ELEMENT * 3)
 );
 
 let result;
@@ -39,89 +39,89 @@ const workerTasks = [];
 const updateRate = 250;
 
 os.cpus().forEach((cpu, idx) => {
-  workerTasks.push({
-    title: "Starting thread " + idx + "...",
-    task: async (ctx, task) => {
-      await new Promise((resolve, reject) => {
-        let attempts = 0;
+    workerTasks.push({
+        title: "Starting thread " + idx + "...",
+        task: async (ctx, task) => {
+            await new Promise((resolve, reject) => {
+                let attempts = 0;
 
-        const worker = new Worker("./worker.js", { workerData });
+                const worker = new Worker("./worker.js", { workerData });
 
-        const interval = setInterval(() => {
-          task.title = "#" + idx + ": " + attempts.toLocaleString() + " hashes";
-        }, updateRate);
+                const interval = setInterval(() => {
+                    task.title = "#" + idx + ": " + attempts.toLocaleString() + " hashes";
+                }, updateRate);
 
-        worker.on("message", (data) => {
-          if ("result" in data) {
-            clearInterval(interval);
+                worker.on("message", (data) => {
+                    if ("result" in data) {
+                        clearInterval(interval);
 
-            result = data.result;
-            resolve(data.result);
-          } else if ("done" in data) {
-            clearInterval(interval);
+                        result = data.result;
+                        resolve(data.result);
+                    } else if ("done" in data) {
+                        clearInterval(interval);
 
-            resolve();
-          } else {
-            attempts = data.attempts;
-          }
-        });
-      });
-    },
-  });
+                        resolve();
+                    } else {
+                        attempts = data.attempts;
+                    }
+                });
+            });
+        },
+    });
 });
 
 workerTasks.push({
-  title: "Total stats",
-  task: async (ctx, task) => {
-    await new Promise((resolve, reject) => {
-      const interval = setInterval(() => {
-        const passed = Date.now() - start;
+    title: "Total stats",
+    task: async (ctx, task) => {
+        await new Promise((resolve, reject) => {
+            const interval = setInterval(() => {
+                const passed = Date.now() - start;
 
-        const total = Atomics.load(workerData.data, 0);
+                const total = Atomics.load(workerData.data, 0);
 
-        rate = (total / (passed / 1000)).toLocaleString(undefined, {
-          minimumFractionDigits: 2,
-          maximumFractionDigits: 2
+                rate = (total / (passed / 1000)).toLocaleString(undefined, {
+                    minimumFractionDigits: 2,
+                    maximumFractionDigits: 2,
+                });
+
+                task.title = total.toLocaleString() + " hashes, " + rate + " hashes/s - press P to pause";
+
+                if (result) {
+                    clearInterval(interval);
+
+                    setTimeout(() => {
+                        console.log("");
+                        console.timeEnd("vanity");
+                        console.log(result.sk + " " + result.pkh);
+                    }, 100);
+
+                    resolve(result);
+                }
+            }, updateRate);
         });
-
-        task.title = total.toLocaleString() + " hashes, " + rate + " hashes/s - press P to pause";
-
-        if (result) {
-          clearInterval(interval);
-
-          setTimeout(() => {
-            console.log("");
-            console.timeEnd("vanity");
-            console.log(result.sk + " " + result.pkh);
-          }, 100);
-
-          resolve(result);
-        }
-      }, updateRate);
-    });
-  },
+    },
 });
 
 const tasks = new Listr(workerTasks, { concurrent: true });
 
-process.stdin.on('keypress', function (ch, key) {
-  if (key && key.ctrl && key.name == 'c') {
-    process.exit();
-  }
-
-  if (key && key.name == 'p') {
-    // toggle pause state
-    const old = Atomics.xor(workerData.data, 1, 1);
-
-    // if old value was "paused",
-    // then new value is "not paused"
-    // -> wake up worker threads
-    if (old == 1) {
-      Atomics.notify(workerData.data, 1);
+process.stdin.on("keypress", function (ch, key) {
+    if (key && key.ctrl && key.name == "c") {
+        process.exit();
     }
-  }
+
+    if (key && key.name == "p") {
+        // toggle pause state
+        const old = Atomics.xor(workerData.data, 1, 1);
+
+        // if old value was "paused",
+        // then new value is "not paused"
+        // -> wake up worker threads
+        if (old == 1) {
+            Atomics.notify(workerData.data, 1);
+        }
+    }
 });
- 
+
 process.stdin.setRawMode(true);
 process.stdin.resume();
 
--- worker.js
+++ worker.js
@@ -11,37 +11,34 @@ let result;
 let pkh = "";
 
 const main = async () => {
-  let found = false;
+    let found = false;
 
-  while (!Atomics.load(workerData.data, 2)) {
-    const ret = Atomics.wait(workerData.data, 1, 1); // 1 = pause
+    while (!Atomics.load(workerData.data, 2)) {
+        const ret = Atomics.wait(workerData.data, 1, 1); // 1 = pause
 
-    if (ret == "not-equal") {
-      Atomics.add(workerData.data, 0, 1); // total attempts
-      attempts++;
-  
-      const privateKey = randomBytes(64);
-      result = await cryptoUtils.extractKeys(sotez.b58cencode(privateKey, sotez.constants.prefix["edsk"]));
-  
-      pkh = result.pkh;
-  
-      parentPort.postMessage({ attempts });
-  
-      if (
-        pkh.startsWith("tz1" + workerData.search) ||
-        pkh.endsWith(workerData.search)
-      ) {
-        Atomics.store(workerData.data, 2, 1); // done
-  
-        found = true;
-        parentPort.postMessage({ result });
-      }
+        if (ret == "not-equal") {
+            Atomics.add(workerData.data, 0, 1); // total attempts
+            attempts++;
+
+            const privateKey = randomBytes(64);
+            result = await cryptoUtils.extractKeys(sotez.b58cencode(privateKey, sotez.constants.prefix["edsk"]));
+
+            pkh = result.pkh;
+
+            parentPort.postMessage({ attempts });
+
+            if (pkh.startsWith("tz1" + workerData.search) || pkh.endsWith(workerData.search)) {
+                Atomics.store(workerData.data, 2, 1); // done
+
+                found = true;
+                parentPort.postMessage({ result });
+            }
+        }
     }
-  }
 
-  if (!found) {
-    parentPort.postMessage({ done: true });
-  }
+    if (!found) {
+        parentPort.postMessage({ done: true });
+    }
 };
 
 main();
